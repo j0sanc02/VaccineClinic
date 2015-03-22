cc77ee8be25d2e6d6a7fb05a34d327e38084b28e
j0sanc02 <j0sanc02@louisville.edu>
2015-03-22 15:21:53 -0400

    Added grant management

---
+++ UniversityofLouisvilleVaccine/Controllers/Grant2Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace UniversityofLouisvilleVaccine.Controllers
+{
+    public class Grant2Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] dirs = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"FileUploads\", "*.*");
+            return View(dirs.ToList());
+        }
+
+        [HttpPost]
+
+        public ActionResult UploadToAppServer()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                HttpPostedFileBase hpf = Request.Files[i];
+                if (hpf.ContentLength == 0)
+                    continue;
+
+                string savedFileName = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory + "FileUploads/",
+                Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public FileStreamResult StreamFileFromAppServer(string fileNameWithExtention)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "FileUploads/";
+            string fileName = fileNameWithExtention;
+            return File(new FileStream(path + fileName, FileMode.Open), "text/plain", fileName);
+        }
+
+        public FilePathResult DownloadFileFromAppServer(string fileNameWithExtention)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "FileUploads/";
+            string fileName = fileNameWithExtention;
+            return File(path + fileName, "text/plain", "test.txt");
+        }
+	}
+}
---
+++ UniversityofLouisvilleVaccine/Controllers/UploadFileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityofLouisvilleVaccine.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = System.IO.Path.GetFileName(file.FileName);
+                    var path = System.IO.Path.Combine(Server.MapPath("~/FileUploads"), fileName);
+                    file.SaveAs(path);
+                }
+                ViewBag.Message = "Upload successful";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ViewBag.Message = "Upload failed";
+                return RedirectToAction("Index");
+            }
+        }
+	}
+}
